b1163d093b83ab1e36f0c07d39632f632502ed25
Kenston O'Neal <koneal2013@gmail.com>
2023-07-31 14:05:37 -0400

    Refactor error handling in key-value store
    Introduced a global var `ErrKeyNotFound` in `key_value_store.go` to replace the usage of `errors.New` in `in_memory_kvs.go` when a key is not found. This change helps in error comparison in tests, making tests less prone to string typos and more maintainable. Confirmed by updating respective test in `in_memory_kvs_test.go`.

--- kvs/key_value_store.go
+++ kvs/key_value_store.go
@@ -1,5 +1,11 @@
 package kvs
 
+import (
+	"errors"
+)
+
+var ErrKeyNotFound = errors.New("key not found")
+
 // KeyValueStore represents the interface for a key-value store.
 type KeyValueStore interface {
 	Get(key string) (string, error)
