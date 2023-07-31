2496eb7be34c4495317448bde0d621c8a41d4495
Kenston O'Neal <koneal2013@gmail.com>
2023-07-31 14:11:59 -0400

    Refactor application's main function to run in-memory key-value store
    The main function in 'main.go' now operates an in-memory key-value store. It now supports commands for reading, writing, and deleting keys, as well as quitting the application. This modification allows users to interact with and test the basic operations of the new key-value store.

--- main.go
+++ main.go
@@ -9,76 +9,35 @@ import (
 
 func main() {
 	store := &kvs.InMemoryKeyValueStore{}
-	txStack := &kvs.TransactionStack{}
 
 	for {
 		fmt.Print("> ")
-
 		var cmd, key, value string
-
-		_, err := fmt.Scan(&cmd)
-		if err != nil {
-			fmt.Println("Error:", err)
-
-			continue
-		}
-
-		// Determine the target for the operation: either the store or the current transaction.
-		var target kvs.KeyValueStore
-		if txStack.Current() != nil {
-			target = txStack.Current()
-		} else {
-			target = store
-		}
+		fmt.Scan(&cmd)
 
 		switch strings.ToUpper(cmd) {
 		case "READ":
-			if _, err = fmt.Scan(&key); err != nil {
-				fmt.Println("Error:", err)
-
-				continue
-			}
-
-			val, err := target.Get(key)
+			fmt.Scan(&key)
+			val, err := store.Get(key)
 			if err != nil {
 				fmt.Println("Error:", err)
 			} else {
 				fmt.Println(val)
 			}
 		case "WRITE":
-			if _, err = fmt.Scan(&key, &value); err != nil {
-				fmt.Println("Error:", err)
-
-				continue
-			}
-
-			if err = target.Put(key, value); err != nil {
+			fmt.Scan(&key, &value)
+			err := store.Put(key, value)
+			if err != nil {
 				fmt.Println("Error:", err)
 			}
 		case "DELETE":
-			if _, err = fmt.Scan(&key); err != nil {
+			fmt.Scan(&key)
+			err := store.Delete(key)
+			if err != nil {
 				fmt.Println("Error:", err)
-
-				continue
-			}
-
-			if err = target.Delete(key); err != nil {
-				fmt.Println("Error:", err)
-			}
-		case "START":
-			txStack.Push()
-		case "COMMIT":
-			if txStack.Current() != nil {
-				txStack.Current().Commit()
-				txStack.Pop()
-			}
-		case "ABORT":
-			if txStack.Current() != nil {
-				txStack.Pop()
 			}
 		case "QUIT":
 			fmt.Println("Exiting...")
-
 			return
 		default:
 			fmt.Println("Unknown command")
