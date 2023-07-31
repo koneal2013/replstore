5cfa9c4ce4b179e984cf9d29657572ba7a489005
Kenston O'Neal <koneal2013@gmail.com>
2023-07-31 14:11:59 -0400

    Refactor application's main function to run in-memory key-value store
    The main function in 'main.go' now operates an in-memory key-value store. It now supports commands for reading, writing, and deleting keys, as well as quitting the application. This modification allows users to interact with and test the basic operations of the new key-value store.

--- main.go
+++ main.go
@@ -2,10 +2,45 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
-	"kv-store/demo"
+	"kv-store/kvs"
 )
 
 func main() {
-	fmt.Println(demo.Message())
+	store := &kvs.InMemoryKeyValueStore{}
+
+	for {
+		fmt.Print("> ")
+		var cmd, key, value string
+		fmt.Scan(&cmd)
+
+		switch strings.ToUpper(cmd) {
+		case "READ":
+			fmt.Scan(&key)
+			val, err := store.Get(key)
+			if err != nil {
+				fmt.Println("Error:", err)
+			} else {
+				fmt.Println(val)
+			}
+		case "WRITE":
+			fmt.Scan(&key, &value)
+			err := store.Put(key, value)
+			if err != nil {
+				fmt.Println("Error:", err)
+			}
+		case "DELETE":
+			fmt.Scan(&key)
+			err := store.Delete(key)
+			if err != nil {
+				fmt.Println("Error:", err)
+			}
+		case "QUIT":
+			fmt.Println("Exiting...")
+			return
+		default:
+			fmt.Println("Unknown command")
+		}
+	}
 }
